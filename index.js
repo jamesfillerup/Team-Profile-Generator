a0c92eb5724ac27c464891b244562ad106b17cf7
jamesfill <jamesp.fillerup@gmail.com>
2022-05-10 16:37:42 -0600

    manager function

--- index.js
+++ index.js
@@ -7,81 +7,84 @@ const Manager = require('./lib/Manager')
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
+let questions = [
+    {
 
-//QUESTIONS TO BUILD TEAM
-function questions () {
-
-    function managerCard (){
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "managerName",
-                message: "What is the manager's name? (Required)",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    }
-                    else{
-                        console.log('Please enter the information required.')
-                        return false;
-                    }
-                }
-                
-            },
-            {
-                type: "input",
-                name: "managerId",
-                message: "What is the manager's ID number? (Required)",
-                validate: idInput => {
-                    if (idInput) {
-                        return true;
-                    }
-                    else{
-                        console.log('Please enter the information required.')
-                        return false;
-                    }
-                }
-                
-            },
-            {
-                type: "input",
-                name: "managerEmail",
-                message: "Describe how you would install your repository? (Required)",
-                validate: emailInput => {
-                    if (emailInput) {
-                        return true;
-                    }
-                    else{
-                        console.log('Please enter the information required.')
-                        return false;
-                    }
-                }
-                
-            },
-            {
-                type: "input",
-                name: "ManagerOffice",
-                message: "What is the manager's office number? (Required)",
-                validate: officeInput => {
-                    if (officeInput) {
-                        return true;
-                    }
-                    else{
-                        console.log('Please enter the information required.')
-                        return false;
-                    }
-                }
-                
-            },
-        ])
     }
-    
-}
+]
 
+function managerCard (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the information required.')
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID number? (Required)",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the information required.')
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email? (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the information required.')
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number? (Required)",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter the information required.')
+                    return false;
+                }
+            }
+            
+        },
+    ]).then(answers => {
+        console.table(answers);
+        let manager = new Manager( answers.name, answers.id, answers.email, answers.officeNumber)
+    })
+};
+managerCard();
 
-
-// TODO: Create a function to write HTML file
 function writeToFile(data) {
 
     fs.writeFile("./dist/index.html", generateHtml(data), err =>{
@@ -91,7 +94,7 @@ function writeToFile(data) {
     })
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer
         .prompt(questions)
